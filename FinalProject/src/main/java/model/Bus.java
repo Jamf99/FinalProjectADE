734432de7fab66461c610342e3cdb6a1a8dfd30d
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2018-11-10 11:05:47 -0500

    Some code

--- FinalProject/src/main/java/model/Bus.java
+++ FinalProject/src/main/java/model/Bus.java
@@ -1,5 +1,43 @@
 package model;
 
+import structures.PriorityQueue;
+
 public class Bus {
+	private PriorityQueue<Boolean, Person> users;
+	private int totalofPasangers;
+	public static int MAX_PASANGERS = 50;
+
+	public Bus() {
+		super();
+		this.users = null;
+		this.totalofPasangers = 0;
+	}
+
+	public void addUsers(Person p) {
+		if (users.isEmpty()) {
+			users = new PriorityQueue();
+			users.insert(p.isDiscapacitado(), p);
+		} else if(users.getWeight()<MAX_PASANGERS) {
+			users.insert(p.isDiscapacitado(), p);
+		}else {
+			
+		}
+	}
+
+	public PriorityQueue getUsers() {
+		return users;
+	}
+
+	public void setUsers(PriorityQueue users) {
+		this.users = users;
+	}
+
+	public int getTotalofPasangers() {
+		return totalofPasangers;
+	}
+
+	public void setTotalofPasangers(int totalofPasangers) {
+		this.totalofPasangers = totalofPasangers;
+	}
 
 }
