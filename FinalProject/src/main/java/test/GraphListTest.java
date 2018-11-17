69d02f5c98be5d60078c6745f5c04ede8352db1a
kevinzarama <zaramaluna1999@hotmail.com>
2018-11-17 09:49:09 -0500

    class GraphList created for do test of class

---
+++ FinalProject/src/main/java/test/GraphListTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import structures.GraphList;
+
+class GraphListTest {
+	
+	private GraphList graphList;
+	
+	private void stage1() {
+		graphList = new GraphList();
+	}
+	
+	@Test
+	public void testAddEdge() {
+		stage1();
+		assertNull(graphList.);
+	}
+
+}
