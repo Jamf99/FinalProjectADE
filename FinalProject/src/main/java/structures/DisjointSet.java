d6c3c097bbf68950abb25f133b72cb827e5c8a5b
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2018-11-25 09:07:48 -0500

    .

--- FinalProject/src/main/java/structures/DisjointSet.java
+++ FinalProject/src/main/java/structures/DisjointSet.java
@@ -2,14 +2,13 @@ package structures;
 
 import java.util.HashSet;
 
-public class DisjointSet<V> extends HashSet<V>{
-	
-	private static final long serialVersionUID = -2115162588983714676L;
+public class DisjointSet<V> extends HashSet<V> {
+
 	protected V parent;
-	
-	public DisjointSet(V x){
+
+	public DisjointSet(V x) {
 		super();
-		parent=x;
+		parent = x;
 		this.add(x);
 	}
 
